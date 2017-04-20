6ff2d1ea764b5e29dcbe84f36c860f4da781dc91
halsarhan <hamza.f.alsarhan.ug@dartmouth.edu>
2017-04-19 20:49:09 -0400

    Login and Register Done

--- ShareRide/LoginViewController.swift
+++ ShareRide/LoginViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginEmailTextField: UITextField!
+    @IBOutlet weak var loginPasswordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,14 +28,55 @@ class LoginViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    @IBAction func loginButtonAction(_ sender: Any) {
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if self.loginEmailTextField.text == "" || self.loginPasswordTextField.text == "" {
+            
+            //Alert to tell the user that there was an error because they didn't fill anythin                               g in the textfields because they didn't fill anything in
+            
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            
+            FIRAuth.auth()?.signIn(withEmail: self.loginEmailTextField.text!, password: self.loginPasswordTextField.text!) { (user, error) in
+                
+                if error == nil {
+                    
+                    //Print into the console if successfully logged in
+                    print("You have successfully logged in")
+                    
+                    
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "mainTabController")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+//                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+
+                   
+                } else {
+                    
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
